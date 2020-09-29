3ab465fefb1681cf4b8a219c8e4c3f38acd2702b
Guilherme Sousa <guilherssousa@gmail.com>
2020-09-29 09:59:19 -0300

    func: var schema = objectToSchema(data);

--- index.js
+++ index.js
@@ -6,29 +6,31 @@ let types = {
 	Date: Date,
 	Map: Map,
 	null: null,
-	Object: {},
+	Object: Object,
 	Array: [],
+}
+
+function typeOf(value) {
+	return Object.prototype.toString.call(value).split(' ')[1].slice(0, -1).toLowerCase();
 }
 
 function castString(str) {
 	return types[str[0].toUpperCase() + str.slice(1)]
 }
 
-function isArray(obj) {
-	return Array.isArray(obj)
-}
-
 const checkObjects = (obj) => {
-	let schema = isArray(obj) ? [] : {};
+	let schema = {};
 	try {
 	    Object.keys(obj).forEach(key => {
-			schema[key] = castString(typeof obj[key])
-			if (typeof obj[key] === 'object') {
-				if(!obj[key] === null) {
-	    			checkObjects(obj[key])
-	    		} else {
-	    			delete schema[key]
-	    		}
+			schema[key] = castString(typeOf(obj[key]))
+			if (typeOf(obj[key]) == 'array') {
+				schema = []
+				let arrayValid = castString(typeOf(obj[key][0]));
+				if (arrayValid === Object) {
+					arrayObjectSchema = checkObjects(obj[key][0]);
+					schema.push(arrayObjectSchema);
+				}
+
 	    	}
 	    })
 	} catch (e) {
@@ -38,17 +40,24 @@ const checkObjects = (obj) => {
     return schema
 }
 
-function objectToSchema(obj) {
+function objectToSchema(obj, undefinedTypeFunction=String) {
 	let schema = {};
 	for (data in obj) {
-		if(typeof obj[data] == 'object') {
+		if(typeOf(obj[data]) == 'object') {
 			if(checkObjects(obj[data]) != null) {
 				schema[data] = checkObjects(obj[data])
 			} else {
 				delete schema[data]
 			}
 		} else {
-			schema[data] = castString(typeof obj[data])
+
+			schema[data] = castString(typeOf(obj[data]))
+			if (schema[data] === undefined) {
+				schema[data] = {
+					type: undefinedTypeFunction,
+					required: false
+				}
+			}
 		}
 	};
 
